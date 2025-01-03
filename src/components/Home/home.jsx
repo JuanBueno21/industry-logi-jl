ddbfae7dd0ba072aa1f9a85a77cfa8a2c508a638
JuanBueno21 <juancamilobuenomintic@gmail.com>
2025-01-03 12:36:00 -0500

    update

---
+++ src/components/Home/home.jsx
@@ -0,0 +1,10 @@
+// import React from "react";
+
+const Home = () => {
+  
+  return (
+    <div>Hola mundo</div>
+  )
+}
+
+export default Home;
---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+import "./navbar.css"
+
+const Navbar = () => {
+
+  return (
+    <div className="navbar-container">
+      <nav className="nav-container">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
